9fdd46ee061be3e4d2eafe440cb9ee8f798529c1
Vinícius Kenji <vinicius.makimoto@gmail.com>
2026-07-25 23:48:56 -0300

    cod ok

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,13 @@
 function Input(props) {
   return (
-    <Input
+    <input
       type={props.type}
       placeholder={props.placeholder}
       className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
       value={props.value}
-      onChange={props.onchange}
+      onChange={props.onChange}
     />
   );
 }
+
 export default Input;
