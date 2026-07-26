bc9929e4807bd0e2f151e9902256994259e7d75f
Vinícius Kenji <vinicius.makimoto@gmail.com>
2026-07-25 23:10:57 -0300

    upload dos arquivos

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useEffect, useState } from "react";
+import { v4 } from "uuid";
+
+function App() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || [],
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  // useEffect(() => {
+  //   async function fetchTasks() {
+  //     //Chamar API com os dados para armazenar os dados no state
+  //     const response = await fetch(
+  //       "https://jsonplaceholder.typicode.com/todos?_limit=10",
+  //       {
+  //         method: "GET",
+  //       },
+  //     );
+
+  //     const data = await response.json();
+  //     setTasks(data);
+  //   }
+  //   //Responsável por trazer os dados da API
+  //   // fetchTasks();
+  // }, []);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      //Atualiza a tarefa
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      //Não precisa atulizar a tarefa
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de tarefas
+        </h1>
+
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
